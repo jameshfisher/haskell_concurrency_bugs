1203da3206b2233e0e3545430ce82e90e5871c92
James Fisher <jameshfisher@gmail.com>
2016-06-06 18:51:26 +0100

    another challenge

--- DeadlockEmpire.hs
+++ DeadlockEmpire.hs
@@ -1,9 +1,10 @@
-module SimpleCounter where
+module DeadlockEmpire where
 
 import qualified Data.IORef as IORef
 import qualified Control.Concurrent as Concurrent
 import qualified Control.Monad as Monad
 import qualified Control.Monad.Extra as MonadExtra
+import Control.Monad.Extra ((&&^))
 
 type Counter = IORef.IORef Int
 
@@ -15,8 +16,8 @@ criticalSection = do
   putStrLn "Entering critical section ..."
   putStrLn "Leaving critical section."
 
-prog :: IO ()
-prog = do
+progSimpleCounter :: IO ()
+progSimpleCounter = do
   counter <- IORef.newIORef 0
   let process n = do
         _ <- Concurrent.forkIO $
@@ -26,3 +27,18 @@ prog = do
         return ()
   process 5
   process 3
+
+progConfusedCounter :: IO ()
+progConfusedCounter = do
+  first <- IORef.newIORef 0
+  second <- IORef.newIORef 0
+  Concurrent.forkIO $ do
+    incrementCounter first
+    incrementCounter second
+    MonadExtra.whenM
+      (fmap (==2) (IORef.readIORef second) &&^ fmap (/=2) (IORef.readIORef first))
+      undefined  -- TODO assert false
+  Concurrent.forkIO $ do
+    incrementCounter first
+    incrementCounter second
+  return ()
